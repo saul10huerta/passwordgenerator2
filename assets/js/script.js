5d61b0b58e3b95daa4d2dc0f2b4f90e214661778
Saul Everardo Huerta Perez <Saul@Sauls-MBP.lan>
2020-10-04 21:02:49 -0500

    added an assets folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 // Assignment code here
-
+function generatePassword() {
+  window.alert("The fight has begun!");
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
