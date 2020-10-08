920c39693c4def6b294ba8f00e6340a8a03b7c2f
Saul Everardo Huerta Perez <Saul@Sauls-MBP.lan>
2020-10-07 21:22:14 -0500

    added generator functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+//generator functions
+function getRandomLower () {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper () {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber () {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSpecial () {
+  const special = '!@#$%^&*(){}[]=<>/,.';
+  return special [Math.floor(Math.random() * special.length)]
+}
+console.log(getRandomSpecial());
+
 // Assignment code here
 function generatePassword() {
   window.alert("The fight has begun!");
