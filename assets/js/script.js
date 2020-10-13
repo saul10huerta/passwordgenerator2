c8e4ce98dd9f8f57d40e3face5f812b9024e53ad
Saul Everardo Huerta Perez <Saul@Sauls-MBP.lan>
2020-10-12 21:50:59 -0500

    added some generate password code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,15 +12,56 @@ function getRandomSpecial () {
   const special = '!@#$%^&*(){}[]=<>/,.';
   return special [Math.floor(Math.random() * special.length)]
 }
-console.log(getRandomSpecial());
+//object for generator functions created
+const randomFunctions = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  special: getRandomSpecial
+};
+
+//DOM elements
+const resultEl = document.getElementById('result');
+const quantityEl = document.getElementById('quantity');
+const lowercaseEl = document.getElementById('lowercase');
+const uppercaseEl = document.getElementById('uppercase');
+const numericEl = document.getElementById('numeric');
+const specialEl = document.getElementById('special');
+const generateBtnEl = document.getElementById('generate');
 
 // Assignment code here
 function generatePassword() {
-  window.alert("The fight has begun!");
-}
+  
+  //get user input from DOM elements
+   //get the number of the string
+  const quantity = +quantityEl.value
+  const isLower = lowercaseEl.checked;
+  const isUpper = uppercaseEl.checked;
+  const isNumeric = numericEl.checked;
+  const isSpecial = specialEl.checked;
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  //create password variable
+  let createdPassword = '';
+  //make sure to know which ones are checked and which are not
+  const boxesChecked = isLower + isNumeric + isUpper + isSpecial;
+  //console.log(boxesChecked);
+
+  //array of the checked boxes
+  const boxesArr = [{ isLower }, { isUpper }, { isNumeric }, { isSpecial }].filter
+  (
+    item => Object.values(item) [0]
+  );
+  console.log(boxesArr);
+
+  //if no boxes are checked then return nothing
+  if(boxesChecked === 0) {
+    return '';
+  }
+
+  //loop over length and call generator function for all types
+  
+  
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -32,4 +73,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtnEl.addEventListener("click", writePassword);
